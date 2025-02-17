f0d3bfb4a45176457a724f94dcbb4534c2f9309c
Julius Njeru <127553760+njeruthuo@users.noreply.github.com>
2025-02-17 17:17:22 +0300

    authentication control

--- src/_layouts/AuthLayout.tsx
+++ src/_layouts/AuthLayout.tsx
@@ -1,17 +1,30 @@
+import { Navigate, Outlet } from "react-router-dom";
+
 import { SwitchLoginSignUp } from "@/_components/navigation";
-import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "@/state/store/store";
 
 const AuthLayout = () => {
+  const isLoggedIn = useSelector(
+    (state: RootState) => state.authReducer.isLoggedIn
+  );
+
   return (
-    <section className="flex justify-center items-center w-full h-screen main-bg">
-      <div className="w-1/3 border shadow-2xl px-4 py-4 rounded-2xl">
-        <div className="flex w-full justify-center">
-          <img src="/logo.jpeg" className="rounded-full h-32" alt="" />
-        </div>
-        <Outlet />
-        <SwitchLoginSignUp />
-      </div>
-    </section>
+    <>
+      {isLoggedIn ? (
+        <Navigate to={"/"} />
+      ) : (
+        <section className="flex justify-center items-center w-full h-screen main-bg">
+          <div className="w-1/3 border shadow-2xl px-4 py-4 rounded-2xl">
+            <div className="flex w-full justify-center">
+              <img src="/logo.jpeg" className="rounded-full h-32" alt="" />
+            </div>
+            <Outlet />
+            <SwitchLoginSignUp />
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 export default AuthLayout;
--- src/_layouts/MainLayout.tsx
+++ src/_layouts/MainLayout.tsx
@@ -1,4 +1,12 @@
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+
+import { RootState } from "@/state/store/store";
+
 const MainLayout = () => {
-  return <div>MainLayout</div>;
+  const isLoggedIn = useSelector(
+    (state: RootState) => state.authReducer.isLoggedIn
+  );
+  return <>{isLoggedIn ? <Outlet /> : <Navigate to={"/sign-in"} />}</>;
 };
 export default MainLayout;
